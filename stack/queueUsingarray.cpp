34bd7523c7a0dd0774b745be307bc6ebc119b606
Hari Shanker Yadav <113604005+Harishankeryadav@users.noreply.github.com>
2023-04-08 21:40:41 +0530

    .

--- stack/queueUsingarray.cpp
+++ stack/queueUsingarray.cpp
@@ -1,3 +1,101 @@
+// #include <iostream>
+// #include <climits>
+// using namespace std;
+
+// class queue_using_array
+// {
+//     int firstindex;
+//     int size;
+//     int *array;
+//     int nextindex;
+//     int arrsize = -1;
+
+// public:
+//     queue_using_array(int size)
+//     {
+//         array = new int(size);
+//         nextindex = 0;
+//         firstindex = 0;
+//         this->size = size;
+//     }
+
+//     void push(int data)
+//     {
+
+//         if (arrsize == size)
+//         {
+//             cout << "In size double " << endl;
+//             int *newarray = new int[2 * size];
+//             int i = 0;
+//             int j = firstindex;
+//             while (firstindex < size)
+//             {
+//                 newarray[i] = array[j];
+//                 i++;
+//                 j++;
+//                 firstindex++;
+//             }
+//             j = 0;
+//             while (j < nextindex)
+//             {
+//                 array[i] = array[j];
+//                 i++;
+//                 j++;
+//             }
+//             firstindex = 0;
+//             nextindex = size;
+//             size = 2 * size;
+//             delete[] array;
+//             array = newarray;
+//         }
+
+//         array[nextindex] = data;
+//         nextindex++;
+//         // nextindex = (nextindex + 1) % arrsize;
+//         // if (nextindex == -1)
+//         // {
+//         //     nextindex = 0;
+//         // }
+//         arrsize++;
+//     }
+
+//     int pop()
+//     {
+//         if (arrsize < 0)
+//         {
+//             cout << "Queue is empty ";
+//             return INT_MIN;
+//         }
+//         int num = array[firstindex];
+//         firstindex++;
+//         arrsize--;
+//         return num;
+//     }
+
+//     int front()
+//     {
+//         if (arrsize < 0)
+//         {
+//             cout << "Queue is empty ";
+//             return INT_MIN;
+//         }
+//         return array[firstindex];
+//     }
+
+//     int getsize()
+//     {
+//         return arrsize + 1;
+//     }
+
+//     bool isEmpty()
+//     {
+//         return size >= 0;
+//     }
+// };
+
+
+
+
 #include <iostream>
 #include <climits>
 using namespace std;
@@ -5,7 +103,7 @@ using namespace std;
 class queue_using_array
 {
     int firstindex;
-    int size;
+    int capacity;
     int *array;
     int nextindex;
     int arrsize = -1;
@@ -16,19 +114,19 @@ public:
         array = new int(size);
         nextindex = 0;
         firstindex = 0;
-        this->size = size;
+        this->capacity = capacity;
     }
 
     void push(int data)
     {
 
-        if (arrsize == size)
+        if (arrsize == capacity)
         {
             cout << "In size double " << endl;
-            int *newarray = new int[2 * size];
+            int *newarray = new int[2 * capacity];
             int i = 0;
             int j = firstindex;
-            while (firstindex < size)
+            while (firstindex < capacity)
             {
                 newarray[i] = array[j];
                 i++;
@@ -43,19 +141,19 @@ public:
                 j++;
             }
             firstindex = 0;
-            nextindex = size;
-            size = 2 * size;
+            nextindex = capacity;
+            capacity = 2 * capacity;
             delete[] array;
             array = newarray;
         }
 
+        if (nextindex == arrsize && arrsize != capacity)
+        {
+            nextindex = 0;
+        }
         array[nextindex] = data;
         nextindex++;
-        // nextindex = (nextindex + 1) % arrsize;
-        // if (nextindex == -1)
-        // {
-        //     nextindex = 0;
-        // }
+
         arrsize++;
     }
 
@@ -89,6 +187,6 @@ public:
 
     bool isEmpty()
     {
-        return size >= 0;
+        return capacity >= 0;
     }
 };
