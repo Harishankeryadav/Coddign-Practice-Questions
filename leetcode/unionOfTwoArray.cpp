ab9ef9e22e042a72de155105ac11c2beadc7f844
hari shanker yadav <harishankeryadav927@gmail.com>
2024-07-05 19:25:45 +0530

    added gfg code

--- leetcode/unionOfTwoArray.cpp
+++ leetcode/unionOfTwoArray.cpp
@@ -1,4 +1,4 @@
-// gfg-> tuf-> easy-> array
+// gfg-> tuf-> array -> easy
 
 #include <iostream>
 #include <vector>
@@ -6,41 +6,40 @@ using namespace std;
 
 vector<int> findUnion(int arr1[], int arr2[], int n, int m)
 {
-    // Your code here
-    // return vector with correct order of elements
     vector<int> v;
-    int j = 0, i = 0, k = 0;
+    int j = 0, i = 0;
     while (i < n && j < m)
     {
         if (arr1[i] <= arr2[j])
         {
-            v.push_back(arr1[i]);
-            if (arr1[i] == arr2[j])
-                j++;
+            if (v.empty() || v.back() != arr1[i] && arr1[i] > v.back())
+                v.push_back(arr1[i]);
             i++;
         }
 
-        if (arr2[j] <= arr1[i])
+        else if (arr2[j] <= arr1[i])
         {
-            v.push_back(arr2[j]);
-            if (arr2[j] == arr1[i])
-                i++;
+            if (v.empty() || v.back() != arr1[i] && arr1[i] > v.back())
+                v.push_back(arr1[i]);
             j++;
         }
-        cout <<"i " <<i <<" j " << j <<endl;
+        else
+        {
+            if (v.empty() || v.back() != arr1[i] && arr1[i] > v.back())
+                v.push_back(arr1[i]);
+            i++, j++;
+        }
     }
 
     for (; i < n; i++)
     {
-        k = v.size() - 1;
-        if (v[k] != arr1[i])
+        if (v.empty() || v.back() != arr1[i] && arr1[i] > v.back())
             v.push_back(arr1[i]);
     }
 
     for (; j < m; j++)
     {
-        k = v.size() - 1;
-        if (v[k] != arr2[j])
+        if (v.empty() || v.back() != arr2[j] && arr1[i] > v.back())
             v.push_back(arr2[j]);
     }
 
@@ -49,10 +48,10 @@ vector<int> findUnion(int arr1[], int arr2[], int n, int m)
 
 int main()
 {
-    int v[] = {1, 2, 3, 4, 5};
-    int v1[] = {1, 2, 3 };
+    int v[] = {1, 2, 3, 0, 4, 5};
+    int v1[] = {1, 2, 3};
 
-    vector<int> vec = findUnion(v, v1, 5, 3);
+    vector<int> vec = findUnion(v, v1, 6, 3);
     for (auto it : vec)
         cout << it << " ";
     return 0;
