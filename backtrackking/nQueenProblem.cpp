33cd427da6ca4f2413300d33dfb081e2d554338d
hari shanker yadav <harishankeryadav927@gmail.com>
2024-07-08 00:53:23 +0530

    for 4 and 8 queen

--- backtrackking/nQueenProblem.cpp
+++ backtrackking/nQueenProblem.cpp
@@ -46,15 +46,15 @@ using namespace std;
 //     }
 // }
 
-int n = 4;
+int n = 8;
 
-vector<string> s = {"....", "....", "....", "...."};
+vector<string> s = {"........", "........", "........", "........", "........", "........", "........", "........"};
 
-int visited[4] = {0}, visited_d1[20] = {0}, visited_d2[20] = {0};
+int visited[8] = {0}, visited_d1[20] = {0}, visited_d2[20] = {0};
 
 bool isPossible(int row, int col)
 {
-    return visited[col] == 0 && visited_d1[row + col] == 0 && visited_d2[n + row - col] == 0;
+    return visited[col] == 0 && visited_d1[row + col] == 0 && visited_d2[n + row - col - 1] == 0;
 }
 
 void nQueen(int row)
@@ -79,13 +79,13 @@ void nQueen(int row)
             s[row][i] = 'Q';
             visited[i] = 1;
             visited_d1[row + i] = 1;
-            visited_d2[n + row - i] = 1;
+            visited_d2[n + row - i - 1] = 1;
 
             nQueen(row + 1);
             s[row][i] = '.';
             visited[i] = 0;
             visited_d1[row + i] = 0;
-            visited_d2[n + row - i] = 0;
+            visited_d2[n + row - i - 1] = 0;
         }
     }
 }
@@ -96,7 +96,7 @@ int main()
     // cout << "enter the size of box ";
     // cin >> n;
     // fun(0);
-    
+
     nQueen(0);
     return 0;
 }
